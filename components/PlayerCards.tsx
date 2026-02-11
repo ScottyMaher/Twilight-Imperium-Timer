4d1dcf01edd042103d865bfd744ceaeaf1217975
Scotty <scottymaher@gmail.com>
2026-02-12 00:25:30 +1000

    Revert "slight performance improvement"
    This reverts commit 73af5a0afb234d6ce884f8e32e3484359dfc00a0.

--- components/PlayerCards.tsx
+++ components/PlayerCards.tsx
@@ -45,7 +45,7 @@ const PlayerCards: React.FC<PlayerCardsProps> = ({
         return (
           <motion.div
             key={player.id}
-            layout="position"
+            layout
             animate={{ scale: sizeScale, opacity }}
             transition={{ duration: 0.3 }}
             className="bg-neutral-500/10 p-2 md:p-4 rounded shadow outline outline-1 outline-neutral-500/30"
